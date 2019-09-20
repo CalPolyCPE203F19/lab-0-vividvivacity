9c48cb02245a8aefdd06bc52ca92cd584d31b670
Viana Chow <vjchow@unix1.csc.calpoly.edu>
2019-09-20 16:32:26 -0700

    all should be working

--- Lab00.java
+++ Lab00.java
@@ -8,29 +8,35 @@ public class Lab00
       double z = 9.8;
 
       // printing the variables
-      System.out.println("x:" + x + ", " + "y:" + y + ", " + "z:" + z);
+      System.out.println("x: " + x + " " + "y: " + y + " " + "z: " + z);
 
       // array in Java
       int[] nums = {3, 6, -1, 2};
-      for (int val : nums)
+      for (int val : nums) {
          System.out.println(val);
+      }
 
       // call a function
-      int numFound = char_count(y, '1');
+      int numFound = char_count(y, 'l');
       System.out.println("Found: " + numFound);
-   
+  
+      // a counting for loop
+      for (int i = 1; i < 11; i++) {
+         System.out.print(i + " ");
+      }
+ 
    }
    // function here
-   public static int char_count(String s, char c)
-   {
+   public static int char_count(String s, char c) {
       int count = 0;
-      for (int i = 0; i < s.length(); i++)
-         // work on this part
-         if (ch == c) {
-            count += 1;
+      char[] chars = s.toCharArray();
+      for (int i = 0; i < chars.length; i++) {
+         if (chars[i] == c) {
+            count ++;
          }
-         return count;  
 
+      }
+      return count;
    }
 
 }
